30fd54b0a14b604279458affa9671f52b5b01b0a
csgsajeewa <csgsajeewa@gmail.com>
2015-08-04 16:30:21 +0530

    Revert "add new lines to Socket call back"
    This reverts commit 2318e0f13df682e602bd1c89c22bf4d5f66e8c56.

--- SocketCallBack.cpp
+++ SocketCallBack.cpp
@@ -1,7 +1,6 @@
 #include "SocketCallBack.h"
 
 SocketCallBack::SocketCallBack() {
-	change change we need a change we need a complete change
 }
 
 SocketCallBack::SocketCallBack(const SocketCallBack& orig) {
